bd6627835d1cc00fcd55e5730526fd262be8fa7d
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-12-09 13:52:36 -0500

    Fixed state so that it is all in one object

--- src/components/Application.js
+++ src/components/Application.js
@@ -48,8 +48,11 @@ function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    // appointments: {}
   });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   const appArray = appointments.map(app => {
       return <Appointment key={app.id} {...app}/>
@@ -57,10 +60,7 @@ function Application(props) {
 
 
 useEffect(() => {
-  axios.get('http://localhost:8001/api/days').then(response => {
-    console.log(response.data);
-    setDays([...response.data])
-  });
+  axios.get('http://localhost:8001/api/days').then(response => setDays(response.data));
 }, []);
 
   return (
@@ -74,8 +74,8 @@ useEffect(() => {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
