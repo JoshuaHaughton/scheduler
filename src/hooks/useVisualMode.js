ae4efb122dd8b701e6f73e45b62d4b6f3f1ee6cc
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-12-10 00:42:23 -0500

    Built and implemented a custom Hook

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+export default function useVisualMode(init) {
+  const [mode, setMode] = useState(init);
+  const [history, setHistory] = useState([init]);
+
+  const transition = (newM, replace = false) => {
+    if(!replace) {
+      setHistory(prev => ([...prev, newM]));
+      setMode(newM);
+    } else {
+      setHistory(prev => {
+        prev.pop();
+        setMode(newM)
+        return [...prev, newM]
+      })
+    }
+    
+
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+    setMode(history[history.length-2]);
+    setHistory(prev => {
+      (prev.pop())
+      return prev;
+    })
+  }
+  }
+
+  return {mode, transition, back};
+}
