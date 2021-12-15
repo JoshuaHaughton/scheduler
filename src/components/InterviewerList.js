f2481e071b2ef4ca21524029be9b3078934fb97f
Joshua Haughton <itsjoshuahaughton@gmail.com>
2021-12-14 21:46:20 -0500

    Added proptypes in order to ensure that the interviewer prop being passed into the InterviewerList is an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import './InterviewerList.scss';
-import classNames from "classnames";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   let interviewers = [];
 
   props.interviewers.map(interviewer => {
@@ -28,3 +28,9 @@ export default function InterviewerList(props) {
     </section>
   );
 };
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
