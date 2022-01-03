6335fd738fbd35fa44ed56a65124973d6b6aedee
Joshua Haughton <88793094+JoshuaHaughton@users.noreply.github.com>
2022-01-03 13:28:52 -0500

    Fixed back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -25,7 +25,7 @@ export default function useVisualMode(init) {
         let out = prev.slice(0, -1);
         return [...out];
       })
-  } else if (history.length > 1 && mode == 'ERROR_DELETE'){
+  } else if (history.length > 1 && mode === 'ERROR_DELETE'){
     setMode(history[history.length-3]);
     setHistory(prev => {
       let out = prev.slice(0, -2);
